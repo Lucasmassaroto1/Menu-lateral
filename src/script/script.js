3f83b8e7338adedec05d0453d7742db155c86697
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2025-02-28 11:18:51 -0300

    Apenas um simples menu lateral

---
+++ src/script/script.js
@@ -0,0 +1,19 @@
+const botao = document.querySelector('.toggle');
+const menuLateral = document.querySelector('.menu-lateral');
+const conteudo = document.querySelector('.conteudo');
+const background = document.querySelector('.background');
+
+botao.addEventListener('click', () =>{
+    botao.classList.toggle('ativo')
+    menuLateral.classList.toggle('ativo')
+    conteudo.classList.toggle('ativo')
+    background.classList.toggle('ativo')
+    document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
+});
+background.addEventListener('click', () =>{
+    botao.classList.remove('ativo')
+    menuLateral.classList.remove('ativo')
+    conteudo.classList.remove('ativo')
+    background.classList.remove('ativo')
+    document.body.style.backgroundColor = '#ecf0f1'
+})
